3ada7171328a71b51390f65c1610c2ea2994fab0
R4M1Z <p4tr0n0.dat@gmail.com>
2020-03-24 00:34:45 +0400

    edit it

--- start.php
+++ start.php
@@ -8,7 +8,7 @@ $pass="";
 $dbname="todo";
 require 'config/dbcon.php';
 $errors = array('dont-match' =>'', 'authE' => '' , 'empty' => '' , 'weakPassword' => '' , 'emailValidate'=>'', 'encode'=>'', 'usernameValidate'=>'', 'usernameTaken'=>'', 'emailTaken'=>'');
-require 'user.php';
+require 'classes.php';
 $user= new User($conn,$errors);
 $todos = new Todos($conn);
 $name=$username=$email=$title=$content="";
