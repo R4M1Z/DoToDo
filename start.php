6d43ea8536c52776347d5b899550f329e59fd840
Ramiz <p4tr0n0.dat@gmail.com>
2020-03-24 01:47:31 +0400

    EDIT ME

--- start.php
+++ start.php
@@ -1,8 +1,10 @@
 <?php
 session_start();
+////////////Edit these lines////////////////////
 $host="";
 $uname="";
 $pass="";
+///////////////////////////////////////////
 require 'config/dbcon.php';
 $errors = array('dont-match' =>'', 'authE' => '' , 'empty' => '' , 'weakPassword' => '' , 'emailValidate'=>'', 'encode'=>'', 'usernameValidate'=>'', 'usernameTaken'=>'', 'emailTaken'=>'');
 require 'classes.php';
